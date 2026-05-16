884e701e3fcebccf4b9214fe6f95eb71d82e966b
Elbert Tse <etse168@gmail.com>
2026-05-16 15:35:23 -0400

    Add action to deploy page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/bill-split-receipt-scanner",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
